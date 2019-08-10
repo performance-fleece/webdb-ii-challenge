4c18b95d7c933137a798e10b969c64d91a699af5
Jeremy Morales <jeremy.morales@gmail.com>
2019-08-10 11:50:21 -0700

    remove unnecessary migration

--- data/migrations/20190810100250_unique-update.js
+++
@@ -1,9 +0,0 @@
-exports.up = function(knex, Promise) {
-  return knex.schema.alterTable('cars', tbl => {
-    tbl.unique('VIN');
-  });
-};
-
-exports.down = function(knex, Promise) {
-  return knex.schema.dropUnique('cars');
-};
